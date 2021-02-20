509c4a1a66b5a94e9555029bc38cb22924fe2441
Dave Thaler <dthaler@ntdev.microsoft.com>
2021-02-19 18:03:21 -0800

    Add more samples that ought to fail verification
    Signed-off-by: Dave Thaler <dthaler@ntdev.microsoft.com>

---
+++ src/mapoverflow.c
@@ -0,0 +1,37 @@
+// Copyright (c) Prevail Verifier contributors.
+// SPDX-License-Identifier: MIT
+typedef unsigned int uint32_t;
+typedef unsigned long uint64_t;
+
+struct ebpf_map {
+    uint32_t type;
+    uint32_t key_size;
+    uint32_t value_size;
+    uint32_t max_entries;
+    uint32_t map_flags;
+    uint32_t inner_map_idx;
+    uint32_t numa_node;
+};
+#define BPF_MAP_TYPE_ARRAY 2
+
+__attribute__((section("maps"), used))
+struct ebpf_map map =
+    {.type = BPF_MAP_TYPE_ARRAY,
+     .key_size = sizeof(int),
+     .value_size = sizeof(uint64_t),
+     .max_entries = 1};
+
+static int (*ebpf_map_update_elem)(struct ebpf_map* map, const void* key,
+                                   const void* value, uint64_t flags) = (void*) 2;
+
+struct ctx;
+
+int func(struct ctx* ctx)
+{
+    uint32_t key = 10;
+    uint64_t value = 0;
+
+    // The following should fail verification since it tries to
+    // write past the size of the array.
+    return ebpf_map_update_elem(&map, &key, &value, 0);
+}

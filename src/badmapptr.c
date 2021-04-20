1487edc06d682d6a9c26e528e64607cccb53d2cf
Dave Thaler <dthaler@ntdev.microsoft.com>
2021-04-19 19:24:24 -0700

    Add negative sample for bad map pointer
    Signed-off-by: Dave Thaler <dthaler@ntdev.microsoft.com>

---
+++ src/badmapptr.c
@@ -0,0 +1,40 @@
+// Copyright (c) Prevail Verifier contributors.
+// SPDX-License-Identifier: MIT
+
+typedef unsigned int uint32_t;
+
+typedef struct _bpf_map_def
+{
+    uint32_t type;
+    uint32_t key_size;
+    uint32_t value_size;
+    uint32_t max_entries;
+    uint32_t map_flags;
+    uint32_t inner_map_idx;
+    uint32_t numa_node;
+} bpf_map_def_t;
+
+typedef void* (*ebpf_map_lookup_elem_t)(bpf_map_def_t* map, void* key);
+#define ebpf_map_lookup_elem ((ebpf_map_lookup_elem_t)1)
+
+#pragma clang section data = "maps"
+bpf_map_def_t test_map = {
+    .type = 1, // BPF_MAP_TYPE_HASH
+    .key_size = sizeof(uint32_t),
+    .value_size = sizeof(uint32_t),
+    .max_entries = 1};
+
+#pragma clang section text = "test"
+int
+test_repro(void* ctx)
+{
+    uint32_t key = 1;
+
+    bpf_map_def_t* map = &test_map;
+
+    // Instead of passing in the correct map pointer, pass in a value past it.
+    // This should fail verification.
+    uint32_t* value = ebpf_map_lookup_elem(map + 1, &key);
+
+    return (value != 0);
+}

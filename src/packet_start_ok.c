7c310a850663e61d31144505565707b9c218eed4
Elazar Gershuni <elazarg@gmail.com>
2021-07-15 15:59:28 +0300

    add test for simple packet access
    Signed-off-by: Elazar Gershuni <elazarg@gmail.com>

---
+++ src/packet_start_ok.c
@@ -0,0 +1,27 @@
+// Copyright (c) Prevail Verifier contributors.
+// SPDX-License-Identifier: MIT
+typedef unsigned int uint32_t;
+typedef unsigned long uint64_t;
+
+struct xdp_md {
+    uint32_t data;
+    uint32_t data_end;
+    uint32_t data_meta;
+    uint32_t _1;
+    uint32_t _2;
+    uint32_t _3;
+};
+
+struct ctx;
+
+__attribute__((section("xdp"), used))
+int read_write_packet_start(struct xdp_md* ctx)
+{
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+    if (data + sizeof(int) > data_end)
+        return 1;
+    int value = *(int*)data;
+    *(int*)data = value + 1;
+    return 0;
+}

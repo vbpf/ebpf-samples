4a3dcd50a463c55dbf4a6da0663b440677bb1ad4
Dave Thaler <dthaler@microsoft.com>
2021-08-09 12:35:18 -0700

    Add samples for tail call tests
    tail_call_bad tries a non-prog-array map and should fail
    tail_call uses the correct type and should succeed
    The samples are otherwise identical
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

---
+++ src/tail_call.c
@@ -0,0 +1,37 @@
+// Copyright (c) Prevail Verifier contributors.
+// SPDX-License-Identifier: MIT
+typedef unsigned int uint32_t;
+
+#define BPF_MAP_TYPE_PROG_ARRAY 3
+
+typedef struct bpf_map {
+    uint32_t type;
+    uint32_t key_size;
+    uint32_t value_size;
+    uint32_t max_entries;
+    uint32_t map_flags;
+    uint32_t inner_map_idx;
+    uint32_t numa_node;
+} bpf_map_def_t;
+
+struct xdp_md;
+
+static long (*bpf_tail_call)(void *ctx, struct bpf_map *prog_array_map, uint32_t index) = (void*) 12;
+
+__attribute__((section("maps"), used)) struct bpf_map map = {
+    BPF_MAP_TYPE_PROG_ARRAY, sizeof(uint32_t), sizeof(uint32_t), 1};
+
+__attribute__((section("xdp_prog"), used)) int
+caller(struct xdp_md* ctx)
+{
+    long error = bpf_tail_call(ctx, &map, 0);
+
+    // bpf_tail_call failed at runtime.
+    return (int)error;
+}
+
+__attribute__((section("xdp_prog/0"), used)) int
+callee(struct xdp_md* ctx)
+{
+    return 42;
+}

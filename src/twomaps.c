3092e40d88fd4431878ab455cef6e9fc7e1e9ea0
Dave Thaler <dthaler@microsoft.com>
2021-10-05 11:09:00 -0700

    Add sample with two maps that differ only by max_entries
    This case comes up in the Cilium DSR code for example
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

---
+++ src/twomaps.c
@@ -0,0 +1,48 @@
+// Copyright (c) Prevail Verifier contributors.
+// SPDX-License-Identifier: MIT
+typedef unsigned int uint32_t;
+typedef unsigned long uint64_t;
+
+struct ebpf_map {
+    uint32_t type;
+    uint32_t key_size;
+    uint32_t value_size;
+    uint32_t max_entries;
+    uint32_t map_flags;
+    uint32_t inner_map_idx;
+    uint32_t numa_node;
+};
+#define BPF_MAP_TYPE_ARRAY 2
+
+__attribute__((section("maps"), used))
+struct ebpf_map map1 =
+    {.type = BPF_MAP_TYPE_ARRAY,
+     .key_size = sizeof(int),
+     .value_size = sizeof(uint64_t),
+     .max_entries = 1};
+
+__attribute__((section("maps"), used))
+struct ebpf_map map2 =
+    {.type = BPF_MAP_TYPE_ARRAY,
+     .key_size = sizeof(int),
+     .value_size = sizeof(uint64_t),
+     .max_entries = 2};
+
+static void* (*bpf_map_lookup_elem)(struct ebpf_map* map, const void* key) = (void*) 1;
+static int (*get_prandom_u32)() = (void*)7;
+
+struct ctx;
+
+int func(struct ctx* ctx)
+{
+    uint32_t rand32 = get_prandom_u32();
+    struct ebpf_map* map = (rand32 & 1) ? &map1 : &map2;
+
+    int key = 10;
+    uint64_t* value = (uint64_t*)bpf_map_lookup_elem(map, &key);
+    if (value == 0)
+        return 0;
+
+    // The following is safe since both maps have the same value size.
+    return (int)*value;
+}

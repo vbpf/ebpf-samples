72642fe3d03b938cf761dfebb5c296b7e0544ecd
Dave Thaler <dthaler@microsoft.com>
2021-10-07 15:18:05 -0700

    Add byteswap sample that uses all the byteswap opcodes
    As defined in
    https://github.com/iovisor/bpf-docs/blob/master/eBPF.md#byteswap-instructions
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

---
+++ src/byteswap.c
@@ -0,0 +1,26 @@
+// Copyright (c) Prevail Verifier contributors.
+// SPDX-License-Identifier: MIT
+
+static int (*get_prandom_u32)() = (void*)7;
+
+struct ctx;
+
+int func(struct ctx* ctx)
+{
+   int rand32 = get_prandom_u32();
+
+    if (rand32 & 0x01) {
+        asm volatile("r0 = le64 r0\nexit");
+    } else if (rand32 & 0x02) {
+        asm volatile("r0 = le32 r0\nexit");
+    } else if (rand32 & 0x04) {
+        asm volatile("r0 = le16 r0\nexit");
+    } else if (rand32 & 0x10) {
+        asm volatile("r0 = be64 r0\nexit");
+    } else if (rand32 & 0x20) {
+        asm volatile("r0 = be32 r0\nexit");
+    } else {
+        asm volatile("r0 = be16 r0\nexit");
+    }
+    return 0;
+}

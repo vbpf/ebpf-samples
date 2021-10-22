bbdf33d0af697c30b31731584694e41710bf199f
Dave Thaler <dthaler@microsoft.com>
2021-10-22 14:47:07 -0700

    Add sample that illustrates problem with two types
    Sample code for https://github.com/vbpf/ebpf-verifier/issues/286
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

---
+++ src/twotypes.c
@@ -0,0 +1,47 @@
+// Copyright (c) Prevail Verifier contributors.
+// SPDX-License-Identifier: MIT
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+
+struct ebpf_map {
+    uint32_t type;
+    uint32_t key_size;
+    uint32_t value_size;
+    uint32_t max_entries;
+    uint32_t map_flags;
+    uint32_t inner_map_idx;
+    uint32_t numa_node;
+};
+#define BPF_MAP_TYPE_ARRAY 2
+
+__attribute__((section("maps"), used))
+struct ebpf_map map =
+    {.type = BPF_MAP_TYPE_ARRAY,
+     .key_size = sizeof(int),
+     .value_size = 1024,
+     .max_entries = 1};
+
+static void* (*bpf_map_lookup_elem)(struct ebpf_map* map, const void* key) = (void*) 1;
+static int (*get_prandom_u32)() = (void*)7;
+
+struct ctx;
+
+int func(struct ctx* ctx)
+{
+    uint32_t rand32 = get_prandom_u32();
+    uint8_t stack_buffer[256] = {0};
+
+    int map_key = 0;
+    uint8_t* map_value = (uint8_t*)bpf_map_lookup_elem(&map, &map_key);
+    if (map_value == 0)
+        return 0;
+
+    uint8_t* ptr;
+    if (rand32 & 1) {
+        ptr = map_value;
+    } else {
+        ptr = stack_buffer + 128;
+    }
+
+    return *ptr;
+}

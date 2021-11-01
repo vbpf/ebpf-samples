1d3cfbab2fd7b09aa6e5f7671691a396614c8530
Dave Thaler <dthaler@microsoft.com>
2021-11-01 13:05:25 -0700

    Add sample illustrating issue with pointing to two stack sections
    This manually illustrates the issue where this program currently
    fails verification.  However this style of assembly is common
    apparently due to register spilling, and the Linux kernel verifier
    can verify this sort of program, so real programs in use today
    do this.
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

---
+++ src/twostackvars.c
@@ -0,0 +1,46 @@
+// Copyright (c) Prevail Verifier contributors.
+// SPDX-License-Identifier: MIT
+typedef unsigned char uint8_t;
+typedef unsigned int uint32_t;
+
+struct ctx;
+
+static int (*get_prandom_u32)() = (void*)7;
+
+int func(struct ctx* ctx)
+{
+    int stack_buffer[16];
+    int *ptr = (int*)0;
+
+    uint32_t rand32 = get_prandom_u32();
+    if (rand32 & 1) {
+        // In this path we want ptr to point to one section
+        // of stack space that is known to be a number, and have
+        // the rest of the stack be unknown.
+        for (int i = 0; i < 8; i++) {
+            stack_buffer[i] = get_prandom_u32();
+        }
+        int index = rand32 % 8;
+        ptr = &stack_buffer[index];
+
+        // Do something with the pointer to force it to be saved in a
+        // register before joining the two paths.
+        ptr[index ^ 1] = 0;
+    } else {
+        // In this path we want ptr to point to a different section
+        // of stack space that is known to be a number, and have
+        // the rest of the stack be unknown.
+        int* stack_buffer2 = &stack_buffer[8];
+        for (int i = 0; i < 8; i++) {
+            stack_buffer2[i] = get_prandom_u32();
+        }
+        ptr = &stack_buffer2[rand32 % 8];
+    }
+
+    // Here we want to dereference the pointer to get a number.
+    // In both paths above, ptr safely points to a number, even
+    // though each part of stack_buffer is not necessarily a number
+    // at this point.
+
+    return *ptr;
+}

ae8ecc4bfad332c22f1cb91c4eaeb8ff4c496461
Alan Jowett <alanjo@microsoft.com>
2022-02-25 11:16:57 -0700

    Fix line info test data
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- asm/map_in_map.asm
+++ asm/map_in_map.asm
@@ -10,26 +10,28 @@
 ;     void* nolocal_lru_map = bpf_map_lookup_elem(&array_of_maps, &outer_key);
        4:	r1 = map_fd 1
        6:	r0 = map_lookup_elem:1(map_fd r1, map_key r2)
-       7:	if r0 == 0 goto +11 <19>
 ; /home/alanjo/ebpf-verifier/ebpf-samples/src/map_in_map.c:38
 ;     if (nolocal_lru_map) {
-       8:	*(u32 *)(r10 - 8) = r6
+       7:	if r0 == 0 goto +11 <19>
 ; /home/alanjo/ebpf-verifier/ebpf-samples/src/map_in_map.c:39
 ;         uint32_t inner_key = 0;
+       8:	*(u32 *)(r10 - 8) = r6
        9:	r2 = r10
       10:	r2 += -8
-      11:	r1 = r0
 ; /home/alanjo/ebpf-verifier/ebpf-samples/src/map_in_map.c:40
 ;         void* ret = bpf_map_lookup_elem(nolocal_lru_map, &inner_key);
+      11:	r1 = r0
       12:	r0 = map_lookup_elem:1(map_fd r1, map_key r2)
-      13:	if r0 != 0 goto +5 <19>
 ; /home/alanjo/ebpf-verifier/ebpf-samples/src/map_in_map.c:41
 ;         if (ret) {
+      13:	if r0 != 0 goto +5 <19>
       14:	r2 = r10
       15:	r2 += -8
-      16:	r1 = map_fd 2
 ; /home/alanjo/ebpf-verifier/ebpf-samples/src/map_in_map.c:44
 ;             ret = bpf_map_lookup_elem(&inner_map, &inner_key);
+      16:	r1 = map_fd 2
       18:	r0 = map_lookup_elem:1(map_fd r1, map_key r2)
+; /home/alanjo/ebpf-verifier/ebpf-samples/src/map_in_map.c:49
+; }
       19:	r0 = 0
       20:	exit

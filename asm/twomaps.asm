2b07753b6573dd8a3ee2e78c6f3c8034f06e980c
Dave Thaler <dthaler@microsoft.com>
2022-03-21 17:03:53 -0700

    btf_parse_line_information fixes
    Fixes due to https://github.com/vbpf/ebpf-verifier/pull/307
    
    Signed-off-by: Dave Thaler <dthaler@microsoft.com>

--- asm/twomaps.asm
+++ asm/twomaps.asm
@@ -8,18 +8,22 @@
        4:	if r0 == 0 goto +2 <7>
        5:	r1 = map_fd 1
        7:	r2 = 10
-       8:	*(u32 *)(r10 - 4) = r2
-       9:	r2 = r10
 ; /home/alanjo/ebpf-verifier/ebpf-samples/src/twomaps.c:41
 ;     int key = 10;
+       8:	*(u32 *)(r10 - 4) = r2
+       9:	r2 = r10
       10:	r2 += -4
-      11:	r0 = map_lookup_elem:1(map_fd r1, map_key r2)
-      12:	r1 = r0
 ; /home/alanjo/ebpf-verifier/ebpf-samples/src/twomaps.c:42
 ;     uint64_t* value = (uint64_t*)bpf_map_lookup_elem(map, &key);
+      11:	r0 = map_lookup_elem:1(map_fd r1, map_key r2)
+      12:	r1 = r0
       13:	r0 = 0
-      14:	if r1 == 0 goto +1 <16>
-      15:	r0 = *(u64 *)(r1 + 0)
 ; /home/alanjo/ebpf-verifier/ebpf-samples/src/twomaps.c:43
 ;     if (value == 0)
+      14:	if r1 == 0 goto +1 <16>
+; /home/alanjo/ebpf-verifier/ebpf-samples/src/twomaps.c:47
+;     return (int)*value;
+      15:	r0 = *(u64 *)(r1 + 0)
+; /home/alanjo/ebpf-verifier/ebpf-samples/src/twomaps.c:48
+; }
       16:	exit

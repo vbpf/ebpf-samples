a19c1f060cf8ca4ef2edd0f0c373c500d6ee64d3
Alan Jowett <alanjo@microsoft.com>
2022-05-06 13:55:49 -0600

    Add negative test case for EBPF_ARGUMENT_TYPE_PTR_TO_MEM ebpf_argument_type_t
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

---
+++ src/ringbuf_uninit.c
@@ -0,0 +1,32 @@
+// Copyright (c) Prevail Verifier contributors.
+// SPDX-License-Identifier: MIT
+
+typedef unsigned int uint32_t;
+typedef unsigned long uint64_t;
+
+struct ebpf_map {
+    uint32_t type;
+    uint32_t key_size;
+    uint32_t value_size;
+    uint32_t max_entries;
+    uint32_t map_flags;
+    uint32_t inner_map_idx;
+    uint32_t numa_node;
+};
+
+#define BPF_MAP_TYPE_RINGBUF 27
+
+static long (*bpf_ringbuf_output)(void *ringbuf, void *data, uint64_t size, uint64_t flags) = (void *) 130;
+
+__attribute__((section("maps"), used))
+struct ebpf_map ring_buffer = {.type = BPF_MAP_TYPE_RINGBUF, .max_entries = 256 * 1024};
+
+int
+test(void* ctx)
+{
+    uint64_t test;
+    // The following call should fail verification as test is not initialized.
+    bpf_ringbuf_output(&ring_buffer, &test, sizeof(test), 0);
+
+    return 0;
+}

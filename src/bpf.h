ac3262d7f3e02e5fa377a7e9984965cb57d8ae7c
Alan Jowett <alanjo@microsoft.com>
2023-06-07 16:18:00 -0700

    Remove dependency on Linux headers.
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- src/bpf.h
+++ src/bpf.h
@@ -5,6 +5,47 @@
 // SPDX-License-Identifier: MIT
 
 #pragma once
-#include <linux/bpf.h>
-#include <bpf/bpf_helpers.h>
+
 #include <stdint.h>
+
+typedef uint8_t __u8;
+typedef uint16_t __u16;
+typedef uint32_t __u32;
+typedef uint64_t __u64;
+
+typedef int8_t __s8;
+typedef int16_t __s16;
+typedef int32_t __s32;
+typedef int64_t __s64;
+
+typedef __u16 __be16;
+typedef __u32 __be32;
+typedef __u64 __be64;
+
+typedef __u32 __wsum;
+
+enum bpf_map_type {
+	BPF_MAP_TYPE_UNSPEC = 0,
+	BPF_MAP_TYPE_HASH = 1,
+	BPF_MAP_TYPE_ARRAY = 2,
+	BPF_MAP_TYPE_PROG_ARRAY = 3,
+	BPF_MAP_TYPE_ARRAY_OF_MAPS = 12,
+	BPF_MAP_TYPE_RINGBUF = 27,
+};
+
+struct xdp_md {
+    uint32_t data;
+    uint32_t data_end;
+    uint32_t data_meta;
+    uint32_t _1;
+    uint32_t _2;
+    uint32_t _3;
+};
+
+typedef struct __sk_buff {
+    uint32_t _[19];
+    uint32_t data;
+    uint32_t data_end;
+} sk_buff;
+
+#include <bpf_helpers.h>

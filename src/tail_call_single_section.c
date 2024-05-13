6337cb14ded00dab94aaea96af10cd1dcab177af
Dave Thaler <dthaler1968@gmail.com>
2024-05-13 10:28:27 -0600

    Add sample that uses multiple programs in the same section
    See https://nakryiko.com/posts/libbpf-v1/#bpf-program-sec-annotation-streamlining
    
    This sample is needed for https://github.com/vbpf/ebpf-verifier/issues/393
    
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

---
+++ src/tail_call_single_section.c
@@ -0,0 +1,26 @@
+// Copyright (c) Prevail Verifier contributors.
+// SPDX-License-Identifier: MIT
+#include "bpf.h"
+
+__attribute__((section(".maps"), used))
+struct {
+    __uint(type, BPF_MAP_TYPE_PROG_ARRAY);
+    __type(key, uint32_t);
+    __type(value, uint32_t);
+    __uint(max_entries, 1);
+} map;
+
+__attribute__((section("xdp"), used)) int
+caller(struct xdp_md* ctx)
+{
+    long error = bpf_tail_call(ctx, &map, 0);
+
+    // bpf_tail_call failed at runtime.
+    return (int)error;
+}
+
+__attribute__((section("xdp"), used)) int
+callee(struct xdp_md* ctx)
+{
+    return 42;
+}

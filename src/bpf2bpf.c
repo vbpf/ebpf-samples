c9ea782c10a057ab7678c42a0c0eff8f8d6a02fb
Dave Thaler <dthaler1968@gmail.com>
2024-11-07 02:40:10 -0800

    Add stack usage to bpf2bpf.c sample
    Signed-off-by: Dave Thaler <dthaler1968@gmail.com>

--- src/bpf2bpf.c
+++ src/bpf2bpf.c
@@ -2,12 +2,46 @@
 // SPDX-License-Identifier: MIT
 #include "bpf.h"
 
-int plus1(int x) {
-    return x + 1;
+#define ARRAY_SIZE 16
+
+__attribute__ ((optnone))
+int add1(int* x) {
+    // Force using stack space in this function.
+    int inner_array[ARRAY_SIZE];
+    inner_array[1] = 1;
+
+    // Modify the caller's stack.
+    int result = *x + inner_array[1];
+
+    *x = result;
+    return result;
 }
 
+#define CALLER_VALUE 3
+
+// This program is intended to return 3 + 1 = 4.
+// If there is any error in execution (say due to a bug in
+// a JIT compiler or interpreter) it will instead return -1.
 __attribute__((section("test"), used))
 __attribute__((optnone))
 int func(void* ctx) {
-    return plus1(3);
+    // Initialize stack space in the caller.
+    int outer_array[ARRAY_SIZE];
+    outer_array[0] = CALLER_VALUE;
+    outer_array[1] = CALLER_VALUE;
+
+    // Ask callee to modify part of the caller's stack.
+    int result = add1(&outer_array[0]);
+
+    // Verify that the caller's stack was modified as requested.
+    if (result != outer_array[0]) {
+       return -1;
+    }
+
+    // Verify that the rest of the caller's stack is unmodified.
+    if (outer_array[1] != CALLER_VALUE) {
+        return -1;
+    }
+
+    return result;
 }

8f0869b0e07970ab369a4fe7b9302f140159cd47
Alan Jowett <alanjo@microsoft.com>
2024-12-16 10:34:37 -0800

    PR feedback
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- src/global_variable.c
+++ src/global_variable.c
@@ -7,7 +7,7 @@ static volatile uint32_t global_var = 0;
 static volatile uint32_t global_var_2 = 0;
 
 int func(void* ctx) {
-    global_var ++;
-    global_var_2 += 2;
+    __sync_fetch_and_add(&global_var, 1);
+    __sync_fetch_and_add(&global_var_2, 2);
     return 0;
 }

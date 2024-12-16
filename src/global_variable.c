a8001c5e9a03be2a2731fa1ff1a4662fbb83dec8
Alan Jowett <alanjo@microsoft.com>
2024-12-16 10:37:21 -0800

    PR feedback
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- src/global_variable.c
+++ src/global_variable.c
@@ -3,6 +3,12 @@
 
 #include "bpf.h"
 
+// This sample demonstrates how to use global variables in BPF programs.
+// Global variables are shared among all threads in the same BPF program and
+// can be used to store global states. They must be declared as `volatile`
+// to prevent the compiler from optimizing them away and must accessed
+// using atomic operations to avoid race conditions.
+
 static volatile uint32_t global_var = 0;
 static volatile uint32_t global_var_2 = 0;
 

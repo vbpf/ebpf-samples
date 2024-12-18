f022799c04e21d16ff4f832d0c1e45625ab71b70
Alan Jowett <alanjo@microsoft.com>
2024-12-18 12:13:55 -0800

    Test for .bss, .rodata, and .data sections
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- src/global_variable.c
+++ src/global_variable.c
@@ -9,11 +9,18 @@
 // to prevent the compiler from optimizing them away and must accessed
 // using atomic operations to avoid race conditions.
 
+// Global variables with initial value of 0 are put into .bss section.
 static volatile uint32_t global_var = 0;
 static volatile uint32_t global_var_2 = 0;
+// Global const variables are put into .rodata section.
+static volatile const uint32_t global_const_var = 10;
+// Global variables with initial value are put into .data section.
+static volatile uint32_t global_array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 int func(void* ctx) {
     __sync_fetch_and_add(&global_var, 1);
     __sync_fetch_and_add(&global_var_2, 2);
+    global_array[0] = global_const_var;
+
     return 0;
 }
